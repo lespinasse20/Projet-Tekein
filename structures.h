eddb1ebb79fc72c0888c83b55b6df1b9b6255016
lespinasse20 <luka.espinasse@gmail.com>
2026-05-13 11:18:18 +0200

    structures

--- structures.h
+++ structures.h
@@ -1,24 +1,25 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
-#define MAX_QUESTIONS 20
-#define MAX_CHAINE 200
+
+#define MAX_CHAINE 100
+#define MAX_QUESTIONS 50
 
 typedef struct {
-    int points_negatif;
-    int multi_reponse;
-    int mode_sequentiel;
-} parametres;
+    int points_negatif;   // 1 pour Oui, 0 pour Non
+    int mode_sequentiel;  // 1 pour Oui, 0 pour Non
+    int multi_reponse;    // 1 pour Oui, 0 pour Non
+} configuration;
 
 typedef struct {
     char enonce[MAX_CHAINE];
     char proposition[4][MAX_CHAINE];
-    int bonne_reponse[4];
+    int bonne_reponse[4]; // 1 si vrai, 0 si faux
 } question;
 
 typedef struct {
-    char nom_du_qcm[50];
-    parametres config;
+    char nom_du_qcm[60];
     int nombre_de_question;
+    configuration config;
     question liste_questions[MAX_QUESTIONS];
 } qcm;
 
