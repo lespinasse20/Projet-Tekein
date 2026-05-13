458a6f20005fdaeafa595e81a6874a3e605e59c5
lespinasse20 <luka.espinasse@gmail.com>
2026-05-13 11:18:39 +0200

    main

--- main.c
+++ main.c
@@ -2,23 +2,28 @@
 #include <stdlib.h>
 #include "structures.h"
 
+// Prototypes
 void modeEnseignant();
 void modeEtudiant();
-void viderBuffer();
 
 int main() {
     int choix = 0;
-    while (choix != 3) {
-        printf("\n--- LOGICIEL QCM ---\n1. Enseignant\n2. Etudiant\n3. Quitter\nChoix : ");
 
-        while (scanf("%d", &choix) != 1) {
-            printf("Erreur. Entrez 1, 2 ou 3 : ");
-            viderBuffer();
+    while (choix != 3) {
+        printf("\n--- LOGICIEL QCM ---\n");
+        printf("1. Enseignant\n2. Etudiant\n3. Quitter\nChoix : ");
+        
+        if (scanf("%d", &choix) != 1) {
+            while(getchar() != '\n'); // vider buffer si lettre tapee
+            continue;
         }
 
-        if (choix == 1) modeEnseignant();
-        else if (choix == 2) modeEtudiant();
+        switch (choix) {
+            case 1: modeEnseignant(); break;
+            case 2: modeEtudiant(); break;
+            case 3: printf("Au revoir !\n"); break;
+            default: printf("Choix invalide.\n");
+        }
     }
-    printf("Au revoir !\n");
     return 0;
 }
