fddd45c98b4a4d82d2f1b63a08f82586edebbc4e
lespinasse20 <luka.espinasse@gmail.com>
2026-05-19 19:50:10 +0200

    structures

--- structures.h
+++ structures.h
@@ -1,26 +1,28 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
-
 #define MAX_CHAINE 100
-#define MAX_QUESTIONS 50
+#define MAX_QUESTIONS 20
 
 typedef struct {
-    int points_negatif;   // 1 pour Oui, 0 pour Non
-    int mode_sequentiel;  // 1 pour Oui, 0 pour Non
-    int multi_reponse;    // 1 pour Oui, 0 pour Non
+    int points_negatif;
+    int mode_sequentiel;
+    int multi_reponse;
 } configuration;
 
 typedef struct {
     char enonce[MAX_CHAINE];
     char proposition[4][MAX_CHAINE];
-    int bonne_reponse[4]; // 1 si vrai, 0 si faux
+    int bonne_reponse[4];
+    int nb_propositions;
+    float points;         // NOUVEAU : Nombre de points attribués à CETTE question
 } question;
 
 typedef struct {
     char nom_du_qcm[60];
+    char categorie[40];   // NOUVEAU : Catégorie du QCM (ex: Informatique, Histoire...)
     int nombre_de_question;
-    configuration config;
     question liste_questions[MAX_QUESTIONS];
+    configuration config;
 } qcm;
 
 #endif
